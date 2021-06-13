673ed7fe76b007d4a4fa1ca7863f8a5491ffcd50
Dabidobido <davidzhong@hotmail.com>
2021-06-13 21:53:44 +0800

    [0.2] Voidwatch
    Block menu packets and do menu interactions script side
    Added functions to buy cells and phase displacers

--- voidwatch/voidwatch.lua
+++ voidwatch/voidwatch.lua
@@ -1,6 +1,6 @@
 _addon.name     = 'voidwatch'
 _addon.author   = 'Dabidobido'
-_addon.version  = '0.1'
+_addon.version  = '0.2'
 _addon.commands = {'vw'}
 
 -- copied lots of code from https://github.com/Muddshuvel/Voidwatch/blob/master/voidwatch.lua
@@ -31,6 +31,10 @@ local cells = {
     ['Phase Displacer'] = 3853,
 }
 
+local voidwatch_officers = {
+	[235] = { option_index = 2, cobalt_unknown1 = 769, rubicund_unknown1 = 770 },
+}
+
 local function leader()
     local self = windower.ffxi.get_player()
     local party = windower.ffxi.get_party()
@@ -58,9 +62,30 @@ local function poke_thing(thing)
 end
 
 local function poke_rift()
-	log('poke rift')
-	wait_for_rift_0x34 = true
-	poke_thing('Planar Rift')
+	local npc = get_mob_by_name('Planar Rift')
+	if npc then
+		log('poke rift')
+		wait_for_rift_0x34 = true
+		poke_thing('Planar Rift')
+	end
+end
+
+local function poke_officer()
+	local npc = get_mob_by_name('Voidwatch Officer')
+	if npc then
+		log('poke officer')
+		wait_for_officer_0x34 = true
+		poke_thing('Voidwatch Officer')
+	end
+end
+
+local function poke_ardrick()
+	local npc = get_mob_by_name('Ardrick')
+	if npc then
+		log('poke_ardrick')
+		wait_for_adrick_0x34 = true
+		poke_thing('Ardrick')
+	end
 end
 
 local function trade_cells()
@@ -112,50 +137,119 @@ local function trade_cells()
     end
 end
 
-local function escape()
-	log('escaping')
-	while escaping do
-		windower.send_command('setkey escape down')
-		coroutine.sleep(.2)
-		windower.send_command('setkey escape up')
-		coroutine.sleep(1)
-	end
-end
-
 local function handle_command(...)
     local args = {...}
     if args[1] == "t" then
         trade_cells()
+	elseif args[1] == "bc" then
+		buying_cobalt = true
+		poke_officer()
+	elseif args[1] == "br" then
+		buying_rubicund = true
+		poke_officer()
+	elseif args[1] == "bp" and args[2] then
+		number_to_buy = tonumber(args[2])
+		if number_to_buy then
+			if number_to_buy >= 1 and number_to_buy <= 99 then
+				poke_ardrick()
+			else
+				log("Number must be between 1 and 99")
+			end
+		end
     else
-        error("unknown command %s":format(cmd))
+        notice('//vw t: trade cells and displacers and start fight')
+		notice('//vw bc: buy 12 cobalt cells from nearby Voidwatch Officer')
+		notice('//vw br: buy 12 rubicund cells from nearby Voidwatch Officer')
+		notice('//vw bp (number): buy (number) phase displacers from Ardrick')
     end
 end
 
-local function parse_outgoing(id, data)
-    if id == 0x5b then
-		if wait_for_rift_0x5b then
-			wait_for_rift_0x5b = false
-			escaping = false
-			log('start fight')
-			local p = packets.parse('outgoing', data)
-			p['Option Index'] = 0x51
-			p['_unknown1'] = 0
-			return packets.build(p)
-		end
-    end
+local function start_fight()
+	log('start fight')
+	local p = packets.new('outgoing', 0x5b, {
+            ['Target'] = npc_id,
+            ['Target Index'] = npc_index,
+			['Menu ID'] = menu_id,
+			['Zone'] = zone,
+			['Option Index'] = 0x51,
+			['_unknown1'] = 0,
+		})
+	packets.inject(p)
+end
+
+local function buy_cell()
+	log('buying cells')
+	local p = packets.new('outgoing', 0x5b, {
+            ['Target'] = npc_id,
+            ['Target Index'] = npc_index,
+			['Menu ID'] = menu_id,
+			['Zone'] = zone,
+			['Option Index'] = voidwatch_officers[zone].option_index,
+			['_unknown1'] = cell_unknown,
+		})
+	packets.inject(p)
+end
+
+local function buy_phase()
+	log('buying ' .. number_to_buy .. ' phase displacers')
+	local p = packets.new('outgoing', 0x5b, {
+            ['Target'] = npc_id,
+            ['Target Index'] = npc_index,
+			['Menu ID'] = menu_id,
+			['Zone'] = zone,
+			['Option Index'] = 1,
+			['_unknown1'] = number_to_buy,
+		})
+	packets.inject(p)
+end
+
+local function parse_menu_data(p)
+	npc_id = p['NPC']
+	npc_index = p['NPC Index']
+	menu_id = p['Menu ID']
+	zone = p['Zone']
 end
 
 local function parse_incoming(id, data)
 	if id == 0x34 then
 		if wait_for_rift_0x34 then
 			wait_for_rift_0x34 = false
-			wait_for_rift_0x5b = true
-			escaping = true
-			coroutine.schedule(escape, 1)
+			local p = packets.parse('incoming', data)
+			parse_menu_data(p)
+			coroutine.schedule(start_fight, 0.1)
+			return true
+		elseif wait_for_officer_0x34 then
+			wait_for_officer_0x34 = false
+			local p = packets.parse('incoming', data)
+			parse_menu_data(p)
+			if voidwatch_officers[zone] then
+				if buying_cobalt then
+					buying_cobalt = false
+					cell_unknown = voidwatch_officers[zone].cobalt_unknown1
+				elseif buying_rubicund then
+					buying_rubicund = false
+					cell_unknown = voidwatch_officers[zone].rubicund_unknown1
+				else
+					cell_unknown = nil
+				end
+				if cell_unknown then
+					coroutine.schedule(buy_cell, 0.1)
+					return true
+				else
+					log("Didn't set buying_cobalt or buying_rubicund")
+				end
+			else
+				log("No info for voidwatch officer in zone " .. zone)
+			end
+		elseif wait_for_adrick_0x34 then
+			wait_for_adrick_0x34 = false
+			local p = packets.parse('incoming', data)
+			parse_menu_data(p)
+			coroutine.schedule(buy_phase, 0.1)
+			return true
 		end
 	end
 end
 
 windower.register_event('addon command', handle_command)
 windower.register_event('incoming chunk', parse_incoming)
-windower.register_event('outgoing chunk', parse_outgoing)
